82fb9041a37abff5247b5f9b01a571fdb518d693
Xander Denecker <xander.denecker@gmail.com>
2019-04-01 15:48:02 +0200

    feat: yeet
    yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet
    
    BREAKING CHANGE: nope
    Closes yeet

---
+++ assets/js/contact.js
@@ -0,0 +1,12 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    document.querySelector("send").addEventListener("click", );
+};
+
+
+function alert(){
+    alert("hello")
+}
