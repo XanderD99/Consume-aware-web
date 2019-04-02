869b206b97cd05848b34001a54d4b0cc139221a8
Unknown <Rutger.coopman@student.howest.be>
2019-04-02 16:09:21 +0200

    [FEATURE] - study material

--- assets/js/study-material.js
+++ assets/js/study-material.js
@@ -5,8 +5,15 @@ document.addEventListener("DOMContentLoaded", init);
 function init() {
     Form();
     document.querySelector("#teacher").addEventListener("click", UnhideTeacherSub);
-    document.querySelector("#intro").addEventListener("click", UnhideIntoSub);
+    document.querySelector("#book").addEventListener("click", UnhideIntoSub);
+    // document.querySelector("#part1").addEventListener("click", UnhidePart1Sub);
+    document.querySelector("#part1").addEventListener("click", e => selectPart("#part1Sub"));
+    document.querySelector("#part2").addEventListener("click", UnhidePart2Sub);
+    document.querySelector("#part3").addEventListener("click", UnhidePart3Sub);
+    document.querySelector("#chapter1").addEventListener("click", UnhideChapter1Sub);
 };
+
+
 
 
 function Form(){
@@ -15,11 +22,31 @@ function Form(){
     instance.open();
 }
 
-
-function UnhideTeacherSub(){
-    document.querySelector("#teacherSub").classList.remove("hide");
+function hide(query) {
+    document.querySelector(query).classList.add("hide");
 }
 
-function UnhideIntoSub(){
-    document.querySelector("#introSub").classList.remove("hide");
+function show(query) {
+    document.querySelector(query).classList.remove("hide");
 }
+
+function selectPart(query) {
+    // Hide ALL parts
+    hide("#part2Sub");
+    hide("#part1Sub");
+    hide("#part3Sub");
+    
+    // show part that we need
+    show(query);
+}
+
+function UnhideChapter1Sub(){
+    show("#chapter1Sub");
+}
+// chill  yeeeeeeeeeeeeeeeeeeeeeet nice man LINUX
+yeeeeeeeeeeeeeeeeeeeeeeeeeet
+// cvkes ga da dan morgen fixen tis hier gedaan 
+// straks bowlingske :p
+ / cooool
+ // jeeeps
+ 
