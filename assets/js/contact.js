a8baa123001ff80cc67038305c0b1eb540ee60a2
Evert Albert <evert.albert@student.howest.be>
2019-04-03 14:59:53 +0200

    [Feature] Contact page now has working javascript

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,10 +3,25 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
+    const submitButton = document.getElementById("submitButton");
+
+    submitButton.addEventListener("click", function(e){
+        e.preventDefault();
+        getFieldInfo();
+    });
 };
 
-
 function alertFunction(){
-
     alert("hello");
+}
+
+function getFieldInfo(){
+    console.log("button clicked")
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+
+    console.log(name + " " + email + " " + subject + " " + message);
 }
