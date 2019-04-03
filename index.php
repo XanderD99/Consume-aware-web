3559a113d43500895f2aae9a7227861886a7e0c7
Evert Albert <evert.albert@student.howest.be>
2019-04-03 11:04:27 +0200

    started beautyfing landig  page

--- index.php
+++ index.php
@@ -4,14 +4,24 @@ include_once("head.php");
 ?>
 		<main class="container">
 			<h3>Importance of consumer awareness</3>
-			<p>In era of globalization, the consumers became a part of processes which in many cases they do not understand, 
+			<div class="row">
+				<div class="col s4">
+				<i class="fas fa-globe-europe"></i>
+				<p>In era of globalization, the consumers became a part of processes which in many cases they do not understand, 
 				neither are aware of. Economic and technological development boosted consumption of goods and services and made 
 				the consumer being perceived by companies and law makers as a non-professional identity, who is a weaker agent 
-				on the market. It is caused by inability of customers to fight for their rights effectively. It shall be especially 
+				on the market.</p>
+      			</div>
+      			<div class="col s4">
+				<p>It is caused by inability of customers to fight for their rights effectively. It shall be especially 
 				underlined, that in the increasingly connected world, with a growing role of e-commerce, the consumer protection 
-				is crucial, as consumers are rapidly adapting their lives to keep pace with digital advance. “Enhancing quality 
-				in innovative higher education about consumer awareness” is an Erasmus+ project, which aims at promotion of consumer 
-				rights protection and awareness </p>
+				is crucial, as consumers are rapidly adapting their lives to keep pace with digital advance.</p>
+      			</div>
+      			<div class="col s4">
+				<p>“Enhancing quality in innovative higher education about consumer awareness” is an Erasmus+ project, 
+				which aims at promotion of consumer rights protection and awareness.</p>
+      			</div>
+			</div>
 		</main>
 <?php
 include_once("tail.php")
