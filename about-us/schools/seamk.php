7aea51ff3a22f7418d2b613c4c64ae7457dc9355
Xander Denecker <xander.denecker@gmail.com>
2019-04-03 15:15:54 +0200

    kweetnie

---
+++ about-us/schools/seamk.php
@@ -0,0 +1,35 @@
+<h2>SeAMK</h2>
+<p>Seinäjoki University of Applied Sciences (SeAMK) is a multidisciplinary institution of HE with almost 5 000 students, established in1992 through a merger of various institutes each of which had a history of more than 100 years. SeAMK is a non-profit government dependent higher education institution (HEI) and a limited company in its organization form. The learning process and the research, development and innovation (RDI) process are the core processes. They are backed by the process of strategic management and leadership and by the support process. Internationality is integrated into all of these processes. The mission of SeAMK is to advance well-being in the region of South Ostrobothnia and at national level. The vision is to be an international institution of HE with an entrepreneurial spirit – best HEI for the student.</p>
+<h4>Education</h4>
+<ul class='list'> 
+    <li class="list-item">Four faculties: School of Business and Culture, School of Food and Agriculture, School of Health Care and Social Work, School of Technology</li>
+    <li class="list-item">19 bachelor and 7 master degree programs with almost 5 000 students</li>
+    <li class="list-item">12 double degree programs in the fields of business and engineering</li>
+    <li class="list-item">Short and intensive programs and Summer Programme conducted in English available for mobility students in all fields</li>
+    <li class="list-item">280 academic and 100 other staff members</li>
+</ul>
+<h4>
+Research, Development and Innovation (RDI)
+</h4>
+<p>
+RDI is an essential aspect of SeAMK’s mission of providing quality education. The RDI work is mainly focused on applied research and development which serves enterprises and other organizations within the region. Students take part in the RDI projects with working life both on national and international level. The RDI volume is 8 million euro from the total 39 million euro budget, 70% of the RDI funding comes from external sources.
+</p>
+
+<h4>
+International Activities
+</h4>
+<p>The International Strategy of SeAMK is based on the international, national and regional policy papers and surveys. SeAMK´s international standard education, RDI as well as co-operation networks bring significant added value for students, enterprises and other organizations especially in the region. Internationality is recognized as a strategic success factor of SeAMK. The aims include:</p>
+
+<ul class="list">
+    <li class="list-item">The staff is internationally competent and committed and the competence is utilized in teaching, RDI functions and regional development.</li>
+    <li class="list-item">SeAMK is educating internationally competent work force for regional, national and international needs.</li>
+    <li class="list-item">Teaching conducted in a foreign language and international degree programmes are competitive in the global education market.</li>
+    <li class="list-item">SeAMK is investing in creating internationally significant research clusters in co-operation with national, European and global partners.</li>
+    <li class="list-item">Consolidated operation in the global HERA is utilized in regional development and publicity in such a way that the region is developed into a development path of international competence and co-operation between European countries and Russia as well as the partnership countries and HEIs in Asia and Latin America.</li>
+</ul>
+
+<p>Partner HEIs are set in four categories after careful selection, evaluation and joint discussions with the partners: 0) potential partners, 1) mobility on faculty level; 2) mobility and projects in teaching and training on the institutional level; 3) strategic partner HEIs with a focus on RDI activity, systematic mobility and development of double/multiple/joint degrees. SeAMK is also a member in field-specific mobility-enhancing networks. SeAMK has partner HEIs in ca. 50 countries, 150 in European countries and 40 partners in Asia, Africa, North and South America and in Russia.</p>
+
+<p>The most important activities include further development of international services for the mobility target groups and international degree students including double degree students; employing international teaching/research staff and visiting lecturers, ensuring that all first and second cycle degree programmes include a study or training period abroad and at least one three month module taught in a foreign language; guaranteeing that all students can achieve a level of international competence; 200 international degree students, 270 incoming and 500 outgoing mobility students/trainees yearly and the proportion of international graduates 10%; participating in international networks and partnerships to enable 25-30 international projects annually; offering Finnish and international students in each education field opportunity to obtain a double/multiple/joint degree in strategic partnerships.</p>
+
+<p>SeAMK utilizes the EQFM quality management and evaluation system. The quality of internationalization processes of SeAMK was evaluated in the International Audit of International Affairs in 2011. SeAMK also participates in the U-Multirank international evaluation. In the international Study Portal survey 2013 SeAMK was evaluated as the best HEI in Finland by the international degree and mobility students.</p>
