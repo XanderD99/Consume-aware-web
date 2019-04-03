ad8f790af19802a737a25dc2a989ecfaa331ab09
Vanwymeersch Michiel <michiel.vanwymeersch@student.howest.be>
2019-04-03 10:52:43 +0200

    removed duplicate html

--- study-material.php
+++ study-material.php
@@ -1,142 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include_once("head.php");
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Study Module Consumee - Consume Aware</title>
+?>
+<main>
+    <ul class="row">
+        <li>
+            <div class="col s4">
+                <div class="card medium">
+                    <div class="card-image">
+                        <img src="/assets/images/goals.jpg">
+                        <span class="card-title">Goals and description</span>
+                    </div>
+                    <div class="card-content">
+                        <p>In here you will find more information about the goals of this project and a short description.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="/study-material/goalsAndDescription.php">More info</a>
+                    </div>
+                </div>
+            </div>
 
-    <!-- reset -->
-    <link rel="stylesheet" href="/assets/css/reset.css" />
-
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-
-    <link rel="stylesheet" href="/assets/css/screen.css" />
-
-    <!-- Font Awesome-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
-</head>
-
-<body>
-    <nav class="cyan">
-        <div class="nav-wrapper px-4">
-            <a href="/" class="brand-logo"> <img src="/assets/images/Consu mee-03.png" height="60" alt="logo"> </a>
-            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i>
-            </a>
-            <ul class="right hide-on-med-and-down">
-                <li><a href="/">Home</a></li>
-                <li><a href="/project/">Project</a></li>
-                <li><a href="/study-material.php">Study material</a></li>
-                <li><a href="/conference.php">Conference</a></li>
-                <li><a href="/about-us/">About us</a></li>
-                <li><a href="/contact.php">Contact</a></li>
-            </ul>
-        </div>
-    </nav>
-
-    <ul class="sidenav" id="mobile-demo">
-        <li><a href="/">Home</a></li>
-        <li><a href="/project/">Project</a></li>
-        <li><a href="/study-material">Study material</a></li>
-        <li><a href="/conference.php">Conference</a></li>
-        <li><a href="/about-us/">About us</a></li>
-        <li><a href="/contact.php">Contact</a></li>
+        </li>
+        <li>
+            <div class="col s4">
+                <div class="card medium">
+                    <div class="card-image">
+                        <img src="/assets/images/student.jpeg">
+                        <span class="card-title">For students</span>
+                    </div>
+                    <div class="card-content">
+                        <p>Click the link to find all the materials for students.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">Go to material</a>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="col s4">
+                <div class="card medium">
+                    <div class="card-image">
+                        <img src="/assets/images/teacher.jpeg">
+                        <span class="card-title">For teachers</span>
+                    </div>
+                    <div class="card-content">
+                        <p>Click the link to find all the materials for teachers.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">Go to material</a>
+                    </div>
+                </div>
+            </div>
+        </li>
     </ul>
-    <main>
-        <ul class="row">
-            <li>
-                <div class="col s4">
-                    <div class="card medium">
-                        <div class="card-image">
-                            <img src="/assets/images/goals.jpg">
-                            <span class="card-title">Goals and description</span>
-                        </div>
-                        <div class="card-content">
-                            <p>In here you will find more information about the goals of this project and a short description.</p>
-                        </div>
-                        <div class="card-action">
-                            <a href="/study-material/goalsAndDescription.php">More info</a>
-                        </div>
-                    </div>
-                </div>
 
-            </li>
-            <li>
-                <div class="col s4">
-                    <div class="card medium">
-                        <div class="card-image">
-                            <img src="/assets/images/student.jpeg">
-                            <span class="card-title">For students</span>
-                        </div>
-                        <div class="card-content">
-                            <p>Click the link to find all the materials for students.</p>
-                        </div>
-                        <div class="card-action">
-                            <a href="#">Go to material</a>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li>
-                <div class="col s4">
-                    <div class="card medium">
-                        <div class="card-image">
-                            <img src="/assets/images/teacher.jpeg">
-                            <span class="card-title">For teachers</span>
-                        </div>
-                        <div class="card-content">
-                            <p>Click the link to find all the materials for teachers.</p>
-                        </div>
-                        <div class="card-action">
-                            <a href="#">Go to material</a>
-                        </div>
-                    </div>
-                </div>
-            </li>
-        </ul>
-
-    </main>
-    <footer class="page-footer cyan">
-        <div class="container">
-            <div class="row">
-                <div class="col l6 s12">
-                    <ul>
-                        <li><a class="grey-text text-lighten-3" href="/about-us/index.php">About us</a></li>
-                        <li><a class="grey-text text-lighten-3" href="/contact.php">Contact us</a></li>
-                    </ul>
-                </div>
-                <div class="col l6 s12">
-                    <ul class="navIconBox ">
-                        <li>
-                            <a class="grey-text text-lighten-3 footerlink" href="https://www.facebook.com/Consume-Aware-584585511743138/">
-                                <i class="fab fa-facebook"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="grey-text text-lighten-3 footerlink" href="https://twitter.com/Consume_Aware">
-                                <i class="fab fa-twitter-square"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="footer-copyright">
-            <div class="container">
-                Copyright © 2019 Consume Aware. All Rights Reserved
-                <a class="grey-text text-lighten-4 right" href="https://ec.europa.eu/programmes/erasmus-plus/programme-guide/part-a_en">
-                    ERASMUS+
-                </a>
-            </div>
-        </div>
-    </footer>
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="/assets/js/script.js"></script>
-</body>
-
-</html> 
+</main>
+<?php
+include_once("tail.php")
+?>
+    
