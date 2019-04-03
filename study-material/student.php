7a8035a30e3bf86bd04ae1ed97d4aa88cec69790
Unknown <Rutger.coopman@student.howest.be>
2019-04-03 15:02:33 +0200

    Auto stash before merge of "development" and "origin/development"

---
+++ study-material/student.php
@@ -0,0 +1,13 @@
+<div id="studentSub" class="hide center-align">
+    <div class="row">
+      <button id="howTo" class="waves-effect waves-teal cyan btn-large">how to </button>
+      <button id="bookStudent" class="waves-effect waves-teal btn-large">book</button>
+      <button id="appStudent" class="waves-effect waves-teal btn-large">app </button>
+    </div>
+    <div class="row">
+      <button id="presentationStudent" class="waves-effect waves-teal btn-large">presentations</button>
+      <button id="e-bookStudent" class="waves-effect waves-teal btn-large">e-book</button>
+      <button id="practialStudent" class="waves-effect waves-teal btn-large">practical case</button>
+    </div>
+  </div>
+  <script src="/assets/js/study-material/student.js"></script>
