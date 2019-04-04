83bb52d7cdcb74584e2a5cea63d084457ee7070d
Vandenkerckhove Jesse <jesse.vandenkerckhove@student.howest.be>
2019-04-04 16:54:28 +0200

    bug fixes

--- assets/js/ebook/ppt.js
+++ assets/js/ebook/ppt.js
@@ -17,7 +17,7 @@ class Ppt {
     }
 
     display() {
-        DFObject.embed(this.location,"#pdfviewer");
+        PDFObject.embed(this.location,"#pdfviewer");
         document.getElementById("title").innerHTML = this.title;
         let downloadd = document.getElementById("down");
         downloadd.href = this.download;
