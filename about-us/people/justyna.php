c0953694ee13c5e6789dbf5153aa6c61cd9f1074
Unknown <vincent.elias@student.howest.be>
2019-04-04 13:40:32 +0200

    Put message to students in bold

--- about-us/people/justyna.php
+++ about-us/people/justyna.php
@@ -21,4 +21,4 @@
 <p><strong>Systemic products and value creation process in healthcare networks</strong> (Journal of Economics and Management ISSN 1732-1948 Vol. 24 (2) 2016, file:///C:/Users/JustynaMatyjasiewicz/Downloads/05.pdf)</p>
 <p><strong>Knowledge transfer in the networks of international healthcare</strong> / (co-author S. Smyczek),  (Review Of Business, Vol. 37, 2016)</p>
 <h4>Message for students</h4>
-<p>Greatness is simply defined as the ability to believe in yourself and ignoring everyone who says you can’t do it.</p>
+<p class="motto">Greatness is simply defined as the ability to believe in yourself and ignoring everyone who says you can’t do it.</p>
