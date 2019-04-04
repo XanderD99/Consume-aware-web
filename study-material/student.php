0fff966fea222db9863e2bb0647750ced797ef34
Tiemen V <tiemen.vermote@student.howest.be>
2019-04-04 14:29:14 +0200

    empty links ready

--- study-material/student.php
+++ study-material/student.php
@@ -6,14 +6,14 @@ include_once "../head.php";
 <main class="container">
 <div id="studentSub" class="center-align">
     <div class="row">
-      <button id="howTo" class="waves-effect waves-teal cyan btn-large study-button">How to</button>
-      <button id="bookStudent" class="waves-effect waves-teal btn-large study-button">Book</button>
-      <button id="appStudent" class="waves-effect waves-teal btn-large study-button">App </button>
+      <a href="#"><button id="howTo" class="waves-effect waves-teal cyan btn-large study-button">How to</button></a>
+      <a href="#"><button id="bookStudent" class="waves-effect waves-teal btn-large study-button">Book</button></a>
+      <a href="#"><button id="appStudent" class="waves-effect waves-teal btn-large study-button">App </button></a>
     </div>
     <div class="row">
-      <button id="presentationStudent" class="waves-effect waves-teal btn-large study-button">Presentations</button>
-      <button id="e-bookStudent" class="waves-effect waves-teal btn-large study-button">E-book</button>
-      <button id="practialStudent" class="waves-effect waves-teal btn-large study-button">Practical case</button>
+    <a href="#"><button id="presentationStudent" class="waves-effect waves-teal btn-large study-button">Presentations</button></a>
+    <a href="#"><button id="e-bookStudent" class="waves-effect waves-teal btn-large study-button">E-book</button></a>
+    <a href="#"><button id="practialStudent" class="waves-effect waves-teal btn-large study-button">Practical case</button></a>
     </div>
   </div>
   <!-- <script src="/assets/js/study-material/student.js"></script> -->
