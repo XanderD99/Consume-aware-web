0b43192cc6baa19394671de2e3d6e156e32e9829
Unknown <pieterjanwatteny@gmail.com>
2019-04-04 13:54:55 +0200

    start of the visual update

--- assets/js/ebook/pdf.js
+++ assets/js/ebook/pdf.js
@@ -8,9 +8,8 @@ class Pdf {
 
     getHtml(partId, chapterId){
         let html = "";
-        html += "<li class='file' data-part='"+partId+"' data-chapter='"+chapterId+"' data-file='"+this.id+"'>";
-        html += "<img src='assets/images/pdflogo.png' class='logobtn' alt='pdfbutton'>";
-        html += "<h4>"+ this.title +"</h4>";
+        html += "<li id='aside_li' class='file' data-part='"+partId+"' data-chapter='"+chapterId+"' data-file='"+this.id+"'>";
+        html += "<img src='assets/images/pdflogo.png' class='logobtn' alt='pdfbutton'>"+"<h4>"+ this.title +"</h4>" ;
         html += "<a id='download' href=''>    </a>";
         html += "</li>";
         return html;
