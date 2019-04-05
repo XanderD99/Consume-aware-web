32b5672f29b4f3216786160a35ad45ef5920614c
Evert Albert <evert.albert@student.howest.be>
2019-04-05 10:47:27 +0200

    teacher stuff

--- study-material/syllabus.php
+++ study-material/syllabus.php
@@ -6,6 +6,11 @@ include "../head.php";
     <h1>Syllabus</h1>
 </div>
 <div class="row center-align">
+
+    <a href="/ebooks.html?part=1">part 1</a><br>
+    <a href="/ebooks.html?part=2">part 2</a><br>
+    <a href="/ebooks.html?part=3">part 3</a><br>
+
     <a href="teacher.php"><button id="back-btn" class="waves-effect waves-teal cyan btn-large logout-btn">Go back</button></a>
 </div>
 </main>
