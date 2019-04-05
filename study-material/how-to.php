fad81f47344799c92d969cdcff8f8c9eaedb9457
Unknown <Rutger.coopman@student.howest.be>
2019-04-05 10:37:27 +0200

    [FEATURE] - correction

--- study-material/how-to.php
+++ study-material/how-to.php
@@ -3,7 +3,7 @@ include "../head.php";
 ?>
 <main class="container">
 <div id="case-studies" class="center-align">
-    <h1>How To</h1>
+    <h1>Instructions</h1>
     <a href="/assets/pdf/Instruction_-_Methodological_Guide_Students.pdf" download="Instruction_-_Methodological_Guide_Students.pdf">Instruction - Methodological Guide Students</a>
 </div>
 <div class="row center-align">
